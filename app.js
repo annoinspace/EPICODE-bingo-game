2617c5ce734123280b1772ca9775a2cb42aa68ff
annoinspace <aneesahalmask@gmail.com>
2022-10-13 15:12:21 +0100

    user board created

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+let numberOfSquares = 76
+
 let createBingoSquares = function () {
   console.log("this is working")
-  let numberOfSquares = 76
+
   for (i = 0; i < numberOfSquares; i++) {
     const newBingoNumContainer = document.createElement("div")
     const bingoNumbersParent = document.getElementById("bingoBoardContainer")
@@ -10,3 +12,29 @@ let createBingoSquares = function () {
   }
 }
 createBingoSquares()
+
+let numberOfUserSquares = 24
+
+let createUserBingoSheet = function () {
+  console.log("this is working")
+
+  for (i = 0; i < numberOfUserSquares; i++) {
+    const userBingo = document.createElement("div")
+    const userBingoParent = document.getElementById("userBoardContainer")
+    userBingo.classList.add("userBingoNumber")
+    userBingo.innerHTML = i + 1
+    userBingoParent.appendChild(userBingo)
+  }
+}
+createUserBingoSheet()
+
+let getRandomInt = function () {
+  let currentSquare = document.querySelectorAll("bingoNumber")[i]
+
+  for (i = 0; i < numberOfSquares; i++) {
+    let randomNumber = Math.ceil(Math.random() * numberOfSquares)
+  }
+}
+
+document.getElementById("randomiseNum").addEventListener("click", getRandomInt)
+// when button is clicked, the numbers are randomised
